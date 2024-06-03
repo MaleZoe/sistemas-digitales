fbd772c95f99c0092d8131346150e27d9c422b0d
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-03 09:29:20 -0300

    Pagina2.php

--- pagina2.php
+++ pagina2.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Captura de datos del form</title>
+</head>
+<body>
+    <?php
+    echo "El nombre ingresado es: " . $_REQUEST['nombre'] . "<br>";
+    echo "La edad ingresada es: " . $_REQUEST['edad'] . "<br>";
 
+    if ($_REQUEST['edad'] < 18) {
+        echo $_REQUEST['nombre'] . " es menor de edad.";
+    } else {
+        echo $_REQUEST['nombre'] . " es mayor de edad.";
+    }
+    ?>
+</body>
+</html>

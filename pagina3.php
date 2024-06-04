378a38e280f38ed0a51bbb88ac06983ef8cf1c3e
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-04 08:21:32 -0300

    pagina3.php

--- pagina3.php
+++ pagina3.php
@@ -1,1 +1,19 @@
+<html>
+<head>
+    <title>Formulario del punto 3</title>
+</head>
+<body>
+<h1>Resultado del formulario</h1>
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nombre = $_POST["nombre"];
+    $deportesSeleccionados = $_POST["deportes"] ?? [];
 
+    $cantidadDeportes = count($deportesSeleccionados);
+
+    echo "$nombre practica $cantidadDeportes deporte(s).";
+}
+?>
+
+</body>
+</html>

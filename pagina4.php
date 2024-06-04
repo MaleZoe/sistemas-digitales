1ec9c2431d3d10966d577c8169a8c969d3ecafc8
MaleZoe <133693731+MaleZoe@users.noreply.github.com>
2024-06-03 21:34:57 -0300

    pagina4.php

--- pagina4.php
+++ pagina4.php
@@ -1,1 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
+    $monto = isset($_POST['monto']) ? $_POST['monto'] : '';
 
+    if ($monto === 'mas-de-10000') {
+        echo "<p>Debe pagar un recargo debido al monto adeudado.</p>";
+    } else {
+        echo "<p>No se requiere recargo.</p>";
+    }
+}
+?>
+</body>
+</html>
